c77a078211fad7b634914a0e2e9008f24d5006e1
Rodion Volovik <rodion.volovik@zenuity.com>
2019-03-25 23:41:15 +0200

    Mandatory part. Need to add 1.numbers formatting, 2.complex numbers solutions, 3.Output of the maximum polynomial degree 4.Check regex

--- computor.py
+++ computor.py
@@ -1,6 +1,5 @@
 import sys
 import re
-import math
 
 """ Class Computor solves polynomials of the second power """
 
@@ -27,14 +26,14 @@ class Computor:
         self.__calculate()
 
     def __check_equation(self):
-        regex = r"^(-\s)?([0-9]*[.]?[0-9]*\s\*\sX\^[0-9]*)(\s[-+]\s[0-9]*[.]?[0-9]*\s\*\sX\^[0-9]*)*\s=\s(-\s)?([0-9]*[.]?[0-9]*\s\*\sX\^[0-9]*)(\s[-+]\s[0-9]*[.]?[0-9]*\s\*\sX\^[0-9]*)*$"
+        regex = r"^(-\s)?([0-9]*[.]?[0-9]*\s\*\sX\s\^\s[0-9]*)(\s[-+]\s[0-9]*[.]?[0-9]*\s\*\sX\s\^\s[0-9]*)*\s=\s(-\s)?([0-9]*[.]?[0-9]*\s\*\sX\s\^\s[0-9]*)(\s[-+]\s[0-9]*[.]?[0-9]*\s\*\sX\s\^\s[0-9]*)*$"
         if re.search(regex, eq) is None:
             self.__error_exit("ERROR: Wrong input format")
 
     def __parse_equation(self):
         pos_equal_sign = self.equation.find(self.CHAR_EQUAL)
         if pos_equal_sign == -1:
-            self.__error_exit("ERROR: Wrong format")
+            self.__error_exit("ERROR: Wrong format3")
         equation_left_array = self.__split_polynomial(self.equation[:pos_equal_sign - 1])
         equation_right_array = self.__split_polynomial(self.equation[pos_equal_sign + 2:])
         for equation_member in equation_left_array:
@@ -42,26 +41,40 @@ class Computor:
         for equation_member in equation_right_array:
             self.__extract_coefficient(equation_member, self.RIGHT)
 
-        simplified = ""
-        simplified = simplified + str(self.coeff[2]) + " * X^2 " if self.coeff[2] else " "
-        simplified = simplified + str(self.coeff[1]) + " * X^1 " if self.coeff[1] else " "
-        simplified = simplified + str(self.coeff[0]) + " * X^0 " if self.coeff[0] else " "
-        print("Simplified eq: " + simplified + " = 0")
+        simplified_equation = ""
+        simplified_equation += self.__output_member(simplified_equation, self.coeff[2], 2)
+        simplified_equation += self.__output_member(simplified_equation, self.coeff[1], 1)
+        simplified_equation += self.__output_member(simplified_equation, self.coeff[0], 0)
+        print("Reduced form: " + simplified_equation + " = 0")
 
     def __calculate(self):
-        if not self.coeff[2]:
+        if not self.coeff[2] and self.coeff[1]:
+            print("Polynomial degree: 1")
+            print("The solution is:")
             result = -1 * self.coeff[0] / self.coeff[1]
             print(result)
+        elif not self.coeff[2] and not self.coeff[1]:
+            if self.coeff[0] == 0:
+                print("All real numbers are solution")
+            else:
+                print("There are no solutions")
         else:
             discriminant = self.coeff[1] ** 2 - 4 * self.coeff[2] * self.coeff[0]
             if discriminant < 0:
-                self.__error_exit("No result")
+                print("There are no real solutions, but complex are:")
+                result1 = str(-1 * self.coeff[1] / (2 * self.coeff[2])) + " + " + str(abs(discriminant**(1.0/2.0)) / (2 * self.coeff[2])) + "i"
+                result2 = str(-1 * self.coeff[1] / (2 * self.coeff[2])) + " - " + str(abs(discriminant**(1.0/2.0)) / (2 * self.coeff[2])) + "i"
+                print(result1)
+                print(result2)
             elif discriminant == 0:
-                print("Result " + str(-1 * self.coeff[1] / 2 * self.coeff[2]))
+                print("Discriminant is equal zero, the solution is:")
+                print(str(-1 * self.coeff[1] / 2 * self.coeff[2]))
             else:
-                result1 = str((-1 * self.coeff[1] + math.sqrt(discriminant)) / (2 * self.coeff[2]))
-                result2 = str((-1 * self.coeff[1] - math.sqrt(discriminant)) / (2 * self.coeff[2]))
-                print("Result " + result1 + ", " + result2)
+                print("Discriminant is strictly positive, the two solutions are:")
+                result1 = str((-1 * self.coeff[1] + discriminant**(1.0/2.0)) / (2 * self.coeff[2]))
+                result2 = str((-1 * self.coeff[1] - discriminant**(1.0/2.0)) / (2 * self.coeff[2]))
+                print(result1)
+                print(result2)
 
     def __split_polynomial(self, polynomial_string):
         polynomial_members = []
@@ -69,22 +82,40 @@ class Computor:
             pos_power_sign = polynomial_string.find(self.CHAR_POWER)
             if pos_power_sign == -1:
                 self.__error_exit("ERROR: Wrong format1")
-            polynomial_members.append(polynomial_string[:pos_power_sign + 2])
-            polynomial_string = polynomial_string[pos_power_sign + 2:]
+            polynomial_members.append(polynomial_string[:pos_power_sign + 3])
+            polynomial_string = polynomial_string[pos_power_sign + 3:]
         return (polynomial_members)
 
     def __extract_coefficient(self, polynomial_member, SIDE):
+        pow_value = 0
         try:
             pow_value = eval(polynomial_member[polynomial_member.find(self.CHAR_POWER) + 1:])
-            if 2 < pow_value or pow_value < 0:
-                self.__error_exit("ERROR: Polynomial power must be in range [0, 2]")
-            try:
-                coeff = eval(polynomial_member[:polynomial_member.find(self.CHAR_ASTERIX) - 1])
-                self.coeff[pow_value] = self.coeff[pow_value] + coeff * SIDE
-            except:
-                self.__error_exit("ERROR: Wrong format2")
         except:
-            self.__error_exit("ERROR: Wrong format3")
+            self.__error_exit("Wrong format1")
+        if 2 < pow_value or pow_value < 0:
+            self.__error_exit("The polynomial degree is stricly greater than 2, I can't solve.")
+        try:
+            coeff = eval(polynomial_member[:polynomial_member.find(self.CHAR_ASTERIX) - 1])
+            self.coeff[pow_value] = self.coeff[pow_value] + coeff * SIDE
+        except:
+            self.__error_exit("Wrong format2")
+
+    def __output_member(self, simplified_equation, member, member_power):
+        member_string = ""
+        if int(member) < 0:
+            member_string = member_string + " - " + str(abs(member))
+        elif int(member) > 0 and not len(simplified_equation):
+            member_string = member_string + str(abs(member))
+        elif int(member) > 0 and len(simplified_equation):
+            member_string = member_string + " + "+ str(abs(member))
+
+        if member_power == 2 and member:
+            member_string = member_string + " * X ^ 2"
+        elif member_power == 1 and member:
+            member_string = member_string + " * X"
+        elif member_power == 0 and not member and not len(simplified_equation):
+            member_string = member_string + "0"
+        return member_string
 
     def __error_exit(self, error_comment):
         print(error_comment)
